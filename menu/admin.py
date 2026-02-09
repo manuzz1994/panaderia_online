1ba92097d000206d1604fa00648eb68726ccbaf1
manuzz1994 <manu.brambilla94@gmail.com>
2026-02-09 14:42:46 -0300

    Modelos Catergoria Y Productos completos con admin configurado.

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Categoria, Producto
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre','producto_count', 'disponible', 'nro_orden')
+    list_filter = ('disponible',)
+    search_fields = ('nombre',)
+    ordering = ('nro_orden', 'nombre')
+    def imagen(self, obj):
+        if obj.imagen:
+            return mark_safe('<img src="%s" width="50" height="50"/>' % obj.imagen.url)
+        return "Sin imagen"
+    imagen.short_description = 'Vista previa'
+    def producto_count(self, obj):
+        return obj.productos.count()
+    producto_count.short_description = 'N° de productos'
+    
+    
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'imagen', 'precio', 'precio_formateado', 'disponible', 'categoria')
+    #prepopulated_fields = {'slug': ('nombre',)}  # Genera slug automáticamente
+    list_filter = ('disponible', 'categoria')
+    search_fields = ('nombre',)
+    list_editable = ('precio', 'disponible')
+    ordering = ('categoria__nombre', 'nombre', 'precio')
+    
+    def imagen(self, obj):
+        if obj.imagen:
+            return mark_safe('<img src="%s" width="50" height="50"/>' % obj.imagen.url)
+        return "Sin imagen"
+    imagen.short_description = 'Vista previa'
+    
+    fieldsets = (
+        ('Información básica', {
+            'fields': ('nombre', 'descripcion','categoria')
+        }),
+        ('Precio y disponibilidad', {
+            'fields': ('precio', 'disponible')
+        }),
+        ('Imagen del producto', {
+            'fields': ('imagen',)
+        }),
+    )
+    
+    def precio_formateado(self, obj):
+        return f"${obj.precio}"
+    precio_formateado.short_description = 'Precio'
+    precio_formateado.admin_order_field = 'precio' # Permite ordenar por precio al hacer clic en el encabezado de la columna
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.text import slugify
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True, verbose_name="Nombre de la categoría")
+    descripcion = models.TextField(blank=True, null=True, verbose_name="Descripción")
+    disponible = models.BooleanField(default=True, verbose_name="¿Disponible?")
+    nro_orden = models.IntegerField(default=0, verbose_name="Número de orden")
+    #SLUG: slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    #Metedo para SLUG= def save(self, *args, **kwargs):
+    #    if not self.slug:
+    #        self.slug = slugify(self.nombre)
+    #    super().save(*args, **kwargs)
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorias"
+        ordering = ['nro_orden', 'nombre', 'disponible']
 
 
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, unique=True, verbose_name="Nombre del producto")
+    descripcion = models.TextField(blank=True, null=True, verbose_name="Descripción")
+    precio = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    disponible = models.BooleanField(default=True, verbose_name="¿Disponible?")
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True, verbose_name="Imagen del producto")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+    #creado = models.DateTimeField(auto_now_add=True)
+    #actualizado = models.DateTimeField(auto_now=True)
+    #SLUG: slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    #Metodo para SLUG= def save(self, *args, **kwargs):
+    #    if not self.slug:
+    #        self.slug = slugify(self.nombre)
+    #    super().save(*args, **kwargs)
+    def __str__(self):
+        return f"{self.nombre} - ${self.precio}"
+    
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+        ordering = ['categoria__nombre', 'nombre', 'precio', 'disponible']
+    
