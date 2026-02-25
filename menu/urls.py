6310b5e7404eda99d300caefdd5ab787829edae6
Manu ZZ <manu.brambilla94@gmail.com>
2026-02-25 16:12:10 -0300

    Agrego urls.py al seguimiento de Git

---
+++ menu/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('categoria/<int:categoria_id>/', views.productos_por_categoria, name='productos_por_categoria'),
+    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
+]
